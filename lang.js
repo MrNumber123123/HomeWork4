28a96252da3e07d8d0d658016873a7162858d598
MrNumber123123 <discorddaccosnova@gmail.com>
2024-11-09 12:01:20 +0200

    Initial commit

---
+++ lang.js
@@ -0,0 +1,32 @@
+const langArr = {
+  title: {
+    ua: "Мульти язычна сторінка",
+    ua: "Мульти язычная страница",
+    en: "Multilingual page",
+  },
+  name: {
+    ua: "Ноутбук Lenovo IdeaPad",
+    ru: "Ноутбук Lenovo IdeaPad",
+    en: "Laptop Levovo IdeaPad",
+  },
+  os: {
+    ua: "Операційна сиситема",
+    ru: "Операционная сиситема",
+    en: "Operation sustem",
+  },
+  memory: {
+    ua: "Об'єм пам'яті",
+    en: "Memory space",
+    ru: "Обьем памяти",
+  },
+  type: {
+    ua: "Тип пам'яті",
+    ru: "Тип памяти",
+    en: "Memory space",
+  },
+  color: {
+    ua: "Колір",
+    ru: "Цвет",
+    en: "Color",
+  },
+};
